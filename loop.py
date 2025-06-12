e2b7a1ab2581cb7914f001278aaa74ccdee3d539
Sujan-2006 <sujan032006@gmail.com>
2025-06-12 14:09:31 +0530

    compound interest calculator using while loop

--- loop.py
+++ loop.py
@@ -1,5 +1,26 @@
-n=input("enter your name:")
-while n=="":
-    print("no name entered")
-    n=input("enter your name:")     #if  ot given then it will be an infinite loop
-print("Hello",n)
+#compound interest calculator 
+
+
+p=0
+r=0
+t=0
+
+while p<=0:
+    p=float(input("enter the principle amount:"))
+    if p<=0:
+        print("principle amount should be greater than 0")
+while r<=0:
+    r=float(input("enter the rate of interest:"))
+    if r<=0:
+        print("rate of interest should be greater than 0")
+while t<=0:
+    t=float(input("enter the time period in years:"))
+    if t<=0:
+        print("time period should be greater than 0")
+
+print(p)
+print(r)
+print(t)
+
+amount=p*pow((1+r/100),t)
+print(f"the amount after {t} years is: {amount:.2f}")
