7166b35b122c2a163c5b9ecd3e4a94e85697fbbe
Sujan-2006 <sujan032006@gmail.com>
2025-06-12 11:25:38 +0530

    temperature conversion program

--- ifelse.py
+++ ifelse.py
@@ -1,11 +1,11 @@
-weight=float(input("enter weight:"))                #weight converting program
-unit=input("kilograms or grams(Kg or g):")
-if unit=="Kg":
-    weight=weight*1000
-    unit="g"
-elif unit=="g":
-    weight=weight/1000
-    unit="Kg"
+temperature=float(input("enter temperature:"))                #temperature conversion program
+unit=input("temperature or Fahrenheit(C or F):")
+if unit=="C":
+    temperature=round((9*temperature)/5+32,2)
+    unit="F"
+elif unit=="F":
+    weight=round((temperature-32)*5/9,2)
+    unit="C"
 else:
     print(f"{unit} is not valid")
-print(f"{round(weight,2)} {unit}")
+print(f"{round(temperature,2)} {unit}")
