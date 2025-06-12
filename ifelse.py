3d0fc987f8b673c8e610059a5e105addf8de9faf
Sujan-2006 <sujan032006@gmail.com>
2025-06-12 11:33:35 +0530

    logical operators using if else

--- ifelse.py
+++ ifelse.py
@@ -1,11 +1,9 @@
-temperature=float(input("enter temperature:"))                #temperature conversion program
-unit=input("temperature or Fahrenheit(C or F):")
-if unit=="C":
-    temperature=round((9*temperature)/5+32,2)
-    unit="F"
-elif unit=="F":
-    weight=round((temperature-32)*5/9,2)
-    unit="C"
+t=-5
+sunny=True
+if t>=28 and sunny:
+    print("it is hot outside")
+elif t<=0 or not sunny:
+    print("it is cold outside")
 else:
-    print(f"{unit} is not valid")
-print(f"{round(temperature,2)} {unit}")
+    print("it is moderate outside")
+     
