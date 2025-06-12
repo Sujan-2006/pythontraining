1850ab58271401e74d7e35a231ccc13e2f7d29c6
Sujan-2006 <sujan032006@gmail.com>
2025-06-12 14:41:26 +0530

    counter timer program using for loop

--- time.py
+++ time.py
@@ -1,10 +1,10 @@
 import time
 
 mytime = int(input("Enter time in seconds: "))
-for i in range(0,mytime):
-    print(i)
-    time.sleep(1)
 for i in range(mytime,0,-1):
-    print(i)
-    time.sleep(1)
+    sec=i%60
+    min=int(i/60)%60
+    hrs=int(i/3600)%24
+    print(f"{hrs:02}:{min:02}:{sec:02}")
+    time.sleep(0.1)
 print("I am SUJAN!!")
