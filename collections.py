4e1d1e60d668c88efb03e88e756883dff8d683b1
Sujan-2006 <sujan032006@gmail.com>
2025-06-12 22:33:39 +0530

    quiz game using list ans tuples

--- collections.py
+++ collections.py
@@ -1,10 +1,49 @@
-#telephone number pad program
+qns=("which is the most famous k-pop girl group?",
+     "who is the most famous among BLACKPINK?",
+     "who is the most followed Indian?",
+     "who has the best voice?",
+     "who is the most followed person in the world?")
 
-a=((1,2,3),
-   (4,5,6),
-   (7,8,9),
-   ("*",0,"#"))
-for i in a:
-    for j in i:
-        print(j,end=" ")
-    print()
+ops=(("a. twice","b. blackpink","c. baby monster"),
+    ("a. lisa","b. jennie","c. rose"),
+    ("a. virat","b. priyanka chopra","c. aliah bhat"),
+    ("a. rose ","b. ariana","c. taylor"),
+    ("a. ronaldo","b. selena","c. messie"))
+
+anss=["b","a","a","b","a"]
+guesses=[]
+score=0
+qnno=0
+for qn in qns:
+    print("------------")
+    print(qn)
+    for op in ops[qnno]:
+        print(op)
+    
+    guess=input("enter (a/b/c):")
+    guesses.append(guess)
+    if guess==anss[qnno]:
+        score+=1
+        print("correct")
+    else:
+        print("incorrect")
+        print(f"{anss[qnno]} is the correct answer")
+    qnno+=1
+
+print("result------")
+print("answer:",end=" ")
+for ans in anss:
+    print(ans,end=" ")
+print()
+print("guess:",end=" ")
+for guess in guesses:
+     print(guess,end=" ")
+print()
+
+score=(int(score/len(qns))*100)
+print(f"your score is {score}%")
+    
+    
+
+
+
