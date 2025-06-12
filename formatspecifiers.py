4c7af00d7f646d93e0f436bbba0ecc8c656a29a9
Sujan-2006 <sujan032006@gmail.com>
2025-06-12 12:39:21 +0530

    format specifiers done

---
+++ formatspecifiers.py
@@ -0,0 +1,18 @@
+a=309372.285
+b=-68669.345
+c=237229.968
+
+print(f"a is {a:10}")
+print(f"b is {b:.2f}")
+print(f"c is {c:.1f}")
+print(f"a is {a:<20}")
+print(f"b is {b:>20}")
+print(f"c is {c:^20}")
+print(f"a is {a:+}")
+print(f"b is {b:+}")
+print(f"c is {c: }")
+print(f"a is {a:,}")
+print(f"b is {b:,.2f}")
+print(f"c is {c:=}")
+
+
