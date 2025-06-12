7c6dce44cd12b45e1e2654da709698cd3b215d26
Sujan-2006 <sujan032006@gmail.com>
2025-06-12 09:42:17 +0530

    variable, data type and type casting

---
+++ varDattypcast.py
@@ -0,0 +1,27 @@
+#strings
+a="sujan"
+print(a)
+print(f"HELLO!! I am {a}")
+
+#integer
+age=19
+print(f"I am {age} years old ")
+
+#float
+cgpa=8.65
+print(f"my CGPA is {cgpa}")
+
+#boolean
+online=False
+if online:
+    print("i am online")
+else:
+    print("i am offline")
+
+#type casting
+cgpa=int(cgpa)
+age=float(age)
+print(cgpa,age)
+name=""
+name=bool(name)
+print(name)
