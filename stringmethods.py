5f3ba9b4a68f52877182090fac8715aea7a0a402
Sujan-2006 <sujan032006@gmail.com>
2025-06-12 12:12:25 +0530

    basic string methods

---
+++ stringmethods.py
@@ -0,0 +1,13 @@
+n="sujan is a good boy"
+#r=len(name)
+#r=n.rfind("z")
+#n=n.capitalize()
+#n=n.upper()
+#n=n.lower()
+#n=n.title()
+#n=n.replace("sujan","sujan kumar")
+#n=n.isalpha()
+#n=n.isdigit()
+r=n.count("o")
+
+print(r)
