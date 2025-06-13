4522bf2f69a62d32d4c35a0a2354e474918dc856
Sujan-2006 <sujan032006@gmail.com>
2025-06-13 14:13:49 +0530

    function using return

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
-def animals(x,y):
-    print(f"my dog name is {x}")
-    print(f"it is a {y}")
-    print()
-animals("jacky","german shepherd")
-animals("pinky","girl")
+def Title(a):
+    return a.title()
+a=Title("beauty and the beast")
+print(a)
+
+  
