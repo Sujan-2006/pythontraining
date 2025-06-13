75c7dbefa6508944a5ec26af3780998b4957dcc9
Sujan-2006 <sujan032006@gmail.com>
2025-06-13 10:39:01 +0530

    number guessing game using random method

--- randommethod.py
+++ randommethod.py
@@ -1,10 +1,29 @@
 import random
-l=1
-h=1000
-options=("rock","paper","scissors")
-#n=random.randint(l,h)
-#op=random.choice(options)
-cards=["2","3","4","5","6","7","8","9","10","K","Q","J"]
-random.shuffle(cards)
+low=1
+high=10
+guesses=0
+running=True
+ans=random.randint(low,high)
 
-print(cards)
+print("NUMBER GUESSING GAME")
+while running:
+    guess=input("enter the guess:")
+    if guess.isdigit():
+        guess=int(guess)
+        guesses+=1
+
+        if guess<low or guess>high:
+            print("the number you guessed is out of the range")
+            print(f"please guess a number between {low} and {high}")
+        elif guess<ans:
+            print("you are nowhere near!!")
+        elif guess>ans:
+            print("you are away!!")
+        else:
+            print("hurrrahhhh!! you are correct ")
+            print(f"no. of guesses is {guesses}")
+            running=False
+
+    else:
+        print("invalid guess")
+        print(f"please guess a number between {low} and {high}")
