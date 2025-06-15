e5558e0c1d5459e0d5a813eff979c36714ddb90f
Sujan-2006 <sujan032006@gmail.com>
2025-06-15 21:11:00 +0530

    arbitrary arguments with args and kwargs

--- functions.py
+++ functions.py
@@ -1,4 +1,8 @@
-def fun(**kwargs):
-   for key,value in kwargs.items():
-       print(f"{key}:{value}")
-print(fun(a="hi",b="I",c="am",d="Sujan"))
+def fun(*args,**kwargs):
+   for arg in args:
+       print(arg,end=" ")
+       print()
+   for value in kwargs.values():
+       print(value,end=" ")
+    #print(f"{kwargs.get('a')}")
+print(fun("naruto","sasuke","sakura","kakashi",a="hi",b="I",c="am",d="Sujan"))
