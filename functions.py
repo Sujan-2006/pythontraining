c702694d4497fc8cf77d4c40e07d1b0e2052ae25
Sujan-2006 <sujan032006@gmail.com>
2025-06-15 19:25:17 +0530

    default arguments in function

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
-def Title(a):
-    return a.title()
-a=Title("beauty and the beast")
-print(a)
+import time
 
-  
+def count(b,a=0):     #default arguments should follow the previous
+    for i in range(a,b+1):
+        print(i)
+        time.sleep(1)
+    print("happy new year")
+count(10)
+count(15,5)    
