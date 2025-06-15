2988ec74e1dcf24059de56545d2ed5b88f4453d0
Sujan-2006 <sujan032006@gmail.com>
2025-06-15 21:03:11 +0530

    arbitrary arguments  kwargs

--- functions.py
+++ functions.py
@@ -1,7 +1,4 @@
-def position(greeting,title,f,l):
-    print(f"{greeting} {title}{f} {l}")
-position("hello",l="watson",title="Ms.",f="Emma")          #keyword argument is followed by the positional argument
-
-
-for i in range(1,11):                #arguments preceeded by an identifier
-    print(i,end="-")  #keyword argument
+def fun(**kwargs):
+   for key,value in kwargs.items():
+       print(f"{key}:{value}")
+print(fun(a="hi",b="I",c="am",d="Sujan"))
