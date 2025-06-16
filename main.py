6a5e62f9d12f1ca7f3e3b2878a4175891313ad90
Sujan-2006 <sujan032006@gmail.com>
2025-06-16 09:24:17 +0530

    modules in python

---
+++ main.py
@@ -0,0 +1,10 @@
+import modules
+
+res=modules.pi
+res=modules.sq(5)
+res=modules.cube(5)
+res=modules.circum(5)
+res=modules.area(5)
+
+
+print(res)
---
+++ modules.py
@@ -0,0 +1,16 @@
+#import math
+#import math as m
+#from math import pi 
+#print(help("modules"))
+
+pi=3.14159
+def sq(x):
+    return x**2
+def cube(x):
+    return x**3
+def circum(r):
+    return 2*pi*r
+def area(r):
+    return pi*r**2
+
+
