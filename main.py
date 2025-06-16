8602561222975c0bf1327fc749fddaea97c89970
Sujan-2006 <sujan032006@gmail.com>
2025-06-16 11:32:41 +0530

    banking program

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
-import modules
+def checkbalance(balance):
+    print(f"your balance is {balance:.2f}")
+def deposit():
+    amount=float(input("enter your amount: "))
+    if amount<=0:
+        print("amount should be greater than 0")
+        return 0
+    else:
+        return amount
 
-res=modules.pi
-res=modules.sq(5)
-res=modules.cube(5)
-res=modules.circum(5)
-res=modules.area(5)
+def withdraw(balance):
+    amount=float(input("enter your amount to be withdrawn: "))
+    if amount<0:
+        print("amount should be greater than 0")
+        return 0
+    elif amount>balance:
+        print("insufficient balance")
+        return 0
+    else:
+        return amount
 
 
-print(res)
+def main():
+    balance=0
+    running=True
+    while running:
+        print("----------------------")
+        print("   Banking Program    ")
+        print("1.Check balance")
+        print("2.Deposit amount")
+        print("4.Withdraw Amount")
+        c=input("enter your choice : ")
+        if c=='1':
+            checkbalance(balance)
+        elif c=='2':
+            balance+=deposit()
+        elif c=='3':
+            balance-=withdraw(balance)
+        elif c=='4':
+            running =False
+        else:
+            print("Invalid option")
+    print("Thank yo!!!")
+if __name__=='__main__':
+    main()
+
+    
