4a4522f47b996901199bfaf76861ef51b41b867e
Sujan-2006 <sujan032006@gmail.com>
2025-06-16 12:19:55 +0530

    slot machine program

--- main.py
+++ main.py
@@ -1,47 +1,68 @@
-def checkbalance(balance):
-    print(f"your balance is {balance:.2f}")
-def deposit():
-    amount=float(input("enter your amount: "))
-    if amount<=0:
-        print("amount should be greater than 0")
-        return 0
-    else:
-        return amount
+import random              #PYTHON SLOT MACHINE PROGRAM
 
-def withdraw(balance):
-    amount=float(input("enter your amount to be withdrawn: "))
-    if amount<0:
-        print("amount should be greater than 0")
-        return 0
-    elif amount>balance:
-        print("insufficient balance")
-        return 0
-    else:
-        return amount
-
-
+def spinrow():
+    symbols=['🍇','🍊','🍎','🍉','🍌']
+    return [random.choice(symbols) for _ in range(3)]
+def printrow(row):
+    print("---------")
+    print("|".join(row))
+    print("---------")
+def getpayout(row,bet):
+    if row[0]==row[1]==row[2]:
+        if row[0]=='🍇':
+            return bet*2
+        elif row[0]=='🍊':
+            return bet
+        elif row[0]=='🍎':
+            return bet*3
+        elif row[0]=='🍉':
+            return bet*5
+        elif row[0]=='🍌':
+            return bet*10
+    return 0
 def main():
-    balance=0
-    running=True
-    while running:
-        print("----------------------")
-        print("   Banking Program    ")
-        print("1.Check balance")
-        print("2.Deposit amount")
-        print("4.Withdraw Amount")
-        c=input("enter your choice : ")
-        if c=='1':
-            checkbalance(balance)
-        elif c=='2':
-            balance+=deposit()
-        elif c=='3':
-            balance-=withdraw(balance)
-        elif c=='4':
-            running =False
+    balance=1000
+    print("---------------------------------")
+    print("     WELCOME TO PYTHON SLOTS     ")
+    print("Symbols: 🍇 🍊 🍎 🍉 🍌 ")
+    print("---------------------------------")
+    while balance>0:
+        print(f"current blance is ${balance}")
+        bet=input("Place your bet amount: ")
+        if not bet.isdigit():
+            print("please enter valid amount")
+            continue
+        bet=int(bet)
+        if bet>balance:
+            print("insufficient balance")
+            continue
+        elif bet<=0:
+            print("bet amount should be greater than or equal to 0")
+            continue
+        
+        balance-=bet
+
+        row=spinrow()
+        print("spinning....\n")
+        printrow(row)
+
+        payout=getpayout(row,bet)
+        if payout>0:
+            print(f"you won ${payout}")
         else:
-            print("Invalid option")
-    print("Thank yo!!!")
+            print("Sorry you lost this match")
+        balance+=payout 
+
+        playagain=input("do you want to spin again?(y/n) : ").lower()
+        if playagain!='y':
+            break
+    print("*********************************************")
+    print(f"GAME OVER !! YOUR FINAL BALANCE IS ${balance}")
+    print("*********************************************")
+        
 if __name__=='__main__':
     main()
+    
+
 
     
