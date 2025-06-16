c444d4289c720eb3efd5ff35a362752128e35378
Sujan-2006 <sujan032006@gmail.com>
2025-06-16 12:43:24 +0530

    encryption decryption program

--- main.py
+++ main.py
@@ -1,68 +1,36 @@
-import random              #PYTHON SLOT MACHINE PROGRAM
+import random
+import string
+chars=list(" "+string.punctuation + string.digits +string.ascii_letters)
+key=chars.copy()
+random.shuffle(key)
+print(f"chars: {chars}")
+print(f"key: {key}")
 
-def spinrow():
-    symbols=['🍇','🍊','🍎','🍉','🍌']
-    return [random.choice(symbols) for _ in range(3)]
-def printrow(row):
-    print("---------")
-    print("|".join(row))
-    print("---------")
-def getpayout(row,bet):
-    if row[0]==row[1]==row[2]:
-        if row[0]=='🍇':
-            return bet*2
-        elif row[0]=='🍊':
-            return bet
-        elif row[0]=='🍎':
-            return bet*3
-        elif row[0]=='🍉':
-            return bet*5
-        elif row[0]=='🍌':
-            return bet*10
-    return 0
-def main():
-    balance=1000
-    print("---------------------------------")
-    print("     WELCOME TO PYTHON SLOTS     ")
-    print("Symbols: 🍇 🍊 🍎 🍉 🍌 ")
-    print("---------------------------------")
-    while balance>0:
-        print(f"current blance is ${balance}")
-        bet=input("Place your bet amount: ")
-        if not bet.isdigit():
-            print("please enter valid amount")
-            continue
-        bet=int(bet)
-        if bet>balance:
-            print("insufficient balance")
-            continue
-        elif bet<=0:
-            print("bet amount should be greater than or equal to 0")
-            continue
-        
-        balance-=bet
+#ENCRYPT
 
-        row=spinrow()
-        print("spinning....\n")
-        printrow(row)
+text=input("enter a message to encrypt : ")
+ciphertext=""
 
-        payout=getpayout(row,bet)
-        if payout>0:
-            print(f"you won ${payout}")
-        else:
-            print("Sorry you lost this match")
-        balance+=payout 
+for letter in text:
+    i=chars.index(letter)
 
-        playagain=input("do you want to spin again?(y/n) : ").lower()
-        if playagain!='y':
-            break
-    print("*********************************************")
-    print(f"GAME OVER !! YOUR FINAL BALANCE IS ${balance}")
-    print("*********************************************")
-        
-if __name__=='__main__':
-    main()
-    
+    ciphertext+=key[i]
+print(f"original message : {text}")
+print(f"encrypted message : {ciphertext}")
+
+
+#DECRYPT
+
+ciphertext=input("enter a message to Decrypt : ")
+text=""
+
+for letter in ciphertext:
+    i=key.index(letter)
+    text+=chars[i]
+
+print(f"encrypted message : {ciphertext}")
+print(f"original message : {text}")
+
 
 
     
