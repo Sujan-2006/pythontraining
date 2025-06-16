fd7720d45ab5b89a4ccba65f74fb95cbb1184f18
Sujan-2006 <sujan032006@gmail.com>
2025-06-16 22:32:26 +0530

    super function updated

--- OOPs.py
+++ OOPs.py
@@ -1,45 +1,36 @@
-class Shape:
-    def __init__(self,color,filled):
-        self.color=color
-        self.filled=filled
-    def describe(self):
-        print(f"It is {self.color} and {'filled' if self.filled else 'not filled'}")
+from abc import ABC, abstractmethod
 
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
 
 class Circle(Shape):
-    def __init__(self,color,filled,radius):
-        super().__init__(color,filled)
-        self.radius=radius
-
-    def decsribe(self):
-        print(f"It is a circle with area of {3.14 * self.radius * self.radius}")
-        super().describe()
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return 3.14 * self.radius ** 2
 
 class Square(Shape):
-    def __init__(self,color,filled,width):
-        super().__init__(color,filled)
-        self.width=width
-    def decsribe(self):
-        print(f"It is a circle with area of {self.width * self.width}cm^2")
-        super().describe()
+    def __init__(self, side):
+        self.side = side
+    def area(self):
+        return self.side ** 2
 
 class Triangle(Shape):
-    def __init__(self,color,filled,width,height):
-        super().__init__(color,filled)
-        self.width=width
-        self.height=height
-    def decsribe(self):
-        print(f"It is a circle with area of {(self.width * self.height)/2}cm^2")
-        super().describe()
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+    def area(self):
+        return self.base * self.height * 0.5
 
-circle=Circle(color="Black",filled=True,radius=6)
-square=Square(color="Blue",filled=False,width=5)
-triangle=Triangle(color="Red",filled=False,width=5,height=7)
-print(circle.color)
-print(square.width)
-print(triangle.height)
-square.describe()
+class Pizza(Circle):
+    def __init__(self, topping, radius):
+        super().__init__(radius)
+        self.topping = topping
+       
 
+shapes = [Circle(3), Square(4), Triangle(5, 7),Pizza("sauce",10)]
 
-
-
+for shape in shapes:
+    print(f"{shape.area()}cm^2")
--- superfunction.py
+++ superfunction.py
@@ -1,41 +1,47 @@
 class Shape:
-    def __init__(self,color,filled):
-        self.color=color
-        self.filled=filled
+    def __init__(self, color, filled):
+        self.color = color
+        self.filled = filled
+
     def describe(self):
         print(f"It is {self.color} and {'filled' if self.filled else 'not filled'}")
 
 
 class Circle(Shape):
-    def __init__(self,color,filled,radius):
-        super().__init__(color,filled)
-        self.radius=radius
+    def __init__(self, color, filled, radius):
+        super().__init__(color, filled)
+        self.radius = radius
 
-    def decsribe(self):
-        print(f"It is a circle with area of {3.14 * self.radius * self.radius}")
+    def describe(self):
+        print(f"It is a circle with area of {3.14 * self.radius * self.radius}cm^2")
         super().describe()
+
 
 class Square(Shape):
-    def __init__(self,color,filled,width):
-        super().__init__(color,filled)
-        self.width=width
-    def decsribe(self):
-        print(f"It is a circle with area of {self.width * self.width}cm^2")
+    def __init__(self, color, filled, width):
+        super().__init__(color, filled)
+        self.width = width
+
+    def describe(self):
+        print(f"It is a square with area of {self.width * self.width}cm^2")
         super().describe()
+
 
 class Triangle(Shape):
-    def __init__(self,color,filled,width,height):
-        super().__init__(color,filled)
-        self.width=width
-        self.height=height
-    def decsribe(self):
-        print(f"It is a circle with area of {(self.width * self.height)/2}cm^2")
+    def __init__(self, color, filled, width, height):
+        super().__init__(color, filled)
+        self.width = width
+        self.height = height
+
+    def describe(self):
+        print(f"It is a triangle with area of {(self.width * self.height) / 2}cm^2")
         super().describe()
 
-circle=Circle(color="Black",filled=True,radius=6)
-square=Square(color="Blue",filled=False,width=5)
-triangle=Triangle(color="Red",filled=False,width=5,height=7)
+
+circle = Circle(color="Black", filled=True, radius=6)
+square = Square(color="Blue", filled=False, width=5)
+triangle = Triangle(color="Red", filled=False, width=5, height=7)
 print(circle.color)
 print(square.width)
 print(triangle.height)
-square.describe()
+circle.describe()
