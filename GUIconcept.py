2348e07f26724b2aa07d3c63fbf79ee0f811556a
Sujan-2006 <sujan032006@gmail.com>
2025-06-17 21:02:42 +0530

    box layout(vertical and horizontal)

--- GUIconcept.py
+++ GUIconcept.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow,QLabel
+from PyQt5.QtWidgets import (QApplication,QMainWindow,QLabel,QWidget,QVBoxLayout,QHBoxLayout,QGridLayout)
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QFont
@@ -8,11 +8,39 @@ from PyQt5.QtCore import Qt
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Anime wa kakkoii!!")
+        self.setWindowTitle("Layout Managers")
         self.setGeometry(100,100,300,300)
         self.setWindowIcon(QIcon("img.jpg"))
+        self.initUI()
 
-    
+    def initUI(self):
+        central_widget=QWidget()
+        self.setCentralWidget(central_widget)
+
+        l1=QLabel("1",self)
+        l2=QLabel("2",self)
+        l3=QLabel("3",self)
+        l4=QLabel("4",self)
+        l5=QLabel("5",self)
+
+        l1.setStyleSheet("Background-color:red;")
+        l2.setStyleSheet("Background-color:green;")
+        l3.setStyleSheet("Background-color:yellow;")
+        l4.setStyleSheet("Background-color:blue;")
+        l5.setStyleSheet("Background-color:orange;")
+
+        hbox=QHBoxLayout()    #vbox -> to print vertically
+        hbox.addWidget(l1) 
+        hbox.addWidget(l2)
+        hbox.addWidget(l3)
+        hbox.addWidget(l4)
+        hbox.addWidget(l5)
+
+        central_widget.setLayout(hbox)
+
+
+        '''
+    #IMAGES
         label=QLabel(self)
         label.setGeometry(0,0,400,100)
         pixmap=QPixmap("imp.jpg")
@@ -23,7 +51,7 @@ class MainWindow(QMainWindow):
                           label.width(),
                           label.height())
 
-'''
+    #LABELS
         label=QLabel("Hello",self)
         label.setFont(QFont("Arial",20))
         label.setGeometry(0,0,400,100)
