db1e4b7dfbb129e2d769e4bd8692c1bf1fbfea21
Sujan-2006 <sujan032006@gmail.com>
2025-06-17 14:40:26 +0530

    alarm clock program using datetime method

--- alarmclock.py
+++ alarmclock.py
@@ -1,20 +1,28 @@
 import datetime
+import time
+import pygame
 
-date = datetime.date(2006, 3, 3)
-today=datetime.date.today()
-time=datetime.time(12, 35, 9)
-now=datetime.datetime.now()
-now=now.strftime("%H:%M:%S %m-%d-%Y")
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+    sound="audio.mp3"
+    running=True
+    while running:
+        current_time=datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+        if current_time==alarm_time:
+            print("Thoongunathu Pooothum daaa naaayeeeee!!🔊🔊")
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound)
+            pygame.mixer.music.play()
 
-print(date)
-print(today)
-print(time)
-print(now)
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+                
+            running=False
+        time.sleep(1)
+        
 
-target_datetime=datetime.datetime(2030, 3, 3, 12, 40, 5)
-current_datetime=datetime.datetime.now()
 
-if target_datetime<current_datetime:
-    print("Target date has already passed")
-else:
-    print("Target date has not passed")
+if __name__=="__main__":
+    alarm_time=input("enter the alarm time(HH:MM:SS): ")
+    set_alarm(alarm_time)
