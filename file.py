e8616d906a0d4f2a92004c9f377d322325a18a61
Sujan-2006 <sujan032006@gmail.com>
2025-06-17 11:58:11 +0530

    writing a .json file

--- file.py
+++ file.py
@@ -1,10 +1,12 @@
+
+import json
+
 txt_data="I like pizza"
-students=["naruto","sasuke","sakura","kakashi"]
-file_path="C:/Users/Sujan/Desktop/test.txt"
+students={"s1":"naruto","s2":"sasuke","s3":"sakura","s4":"kakashi"}
+file_path="C:/Users/Sujan/Desktop/test.json"
 try:
     with open(file_path,"w") as file:            #To write an unexisted file
-        for student in students:
-            file.write(student+"\n")                   #a-> to append an existing file
-        print(f"txt file '{file_path}' is created")
+        json.dump(students,file,indent=4)                  #a-> to append an existing file
+        print(f"json file '{file_path}' is created")
 except FileExistsError:
     print("that file already exists")
