fabb57aae525122bf8a417b52eb4711f2b9faa1e
Sujan-2006 <sujan032006@gmail.com>
2025-06-17 12:25:55 +0530

    reading a .txt file

--- file.py
+++ file.py
@@ -1,18 +1,9 @@
-
-import csv
-
-txt_data="I like pizza"
-students=[["name","age","gender"],
-          ["SUJAN",19,"male"],
-          ["MATHEW",20,"male"],
-          ["RESHMA",19,"female"],
-          ["VIDULA",18,"female"]]
-file_path="C:/Users/Sujan/Desktop/test.csv"
+file_path="C:/Users/Sujan/Desktop/test.txt"
 try:
-    with open(file_path,"w",newline="") as file:            #To write an unexisted file
-        writer=csv.writer(file)          
-        for row in students:
-            writer.writerow(row)        #a-> to append an existing file
-        print(f"csv file '{file_path}' is created")
-except FileExistsError:
-    print("that file already exists")
+    with open(file_path,"r") as file:
+        content=file.read()
+        print(content)
+except FileNotFoundError:
+    print("that file is not found")
+except PermissionError:
+    print("you don't have permission to this file")
