04913e434aa589f99663d150d19104e7e92cec29
Sujan-2006 <sujan032006@gmail.com>
2025-06-17 09:39:35 +0530

    property getter,setter and deleter methods

--- OOPs.py
+++ OOPs.py
@@ -1,3 +1,4 @@
+'''
 class Staff:
 
     count=0
@@ -30,3 +31,53 @@ print(Staff.getcount())
 
 print(Staff.get_noofdegree())
 
+'''
+
+class Rectangle:
+    def __init__(self,width,height):
+        self._width=width
+        self._height=height
+
+    @property
+    def width(self):
+        return f"the width is {self._width:.1f}"
+    
+    @property
+    def height(self):
+        return f"the height is {self._height:.1f}"
+    
+    @width.setter
+    def width(self,new_width):
+        if new_width>0:
+            self._width=new_width
+        else:
+            print("width must me greater than 0")
+    @height.setter
+    def height(self,new_height):
+        if new_height>0:
+            self._height=new_height
+        else:
+            print("height must me greater than 0")
+
+
+    @width.deleter
+    def  width(self):
+        del self._width
+        print("Width is deleted")  
+
+    @height.deleter
+    def  height(self):
+        del self._height
+        print("height is deleted")  
+
+
+r1=Rectangle(3,5)
+
+r1.width=5
+r1.height=7
+
+del r1.width
+del r1.height
+
+#print(r1.width)
+#print(r1.height)
